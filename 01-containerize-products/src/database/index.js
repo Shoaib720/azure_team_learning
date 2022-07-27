f21c26bd7badd47e2b4f9183d20f376184105b2a
Shoaib720 <shoaibsshaikh1999@gmail.com>
2022-07-27 13:14:39 +0530

    removed

--- 01-containerize-products/src/database/index.js
+++
@@ -1,4 +0,0 @@
-export { sequelize } from './connection.js'
-// export { esClient } from './connection.js';
-export { ProductRepository } from './repositories/product.repository.js';
-// export { ESRepository } from './repositories/elasticsearch.repository.js'
--- 01-containerize-products/src/services/index.js
+++
@@ -1,3 +0,0 @@
-export { ProductService } from './product.service.js';
-// export { QueueService } from './queue.service.js';
-// export { ESService } from './elasticsearch.service.js';
